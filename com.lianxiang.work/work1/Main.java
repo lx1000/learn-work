218ae26fca3da2ac6b8e85ad8d62a6e8a9f6cb30
李响 <zoroikuzo@qq.com>
2021-08-17 16:27:58 +0800

    hello world

---
+++ com.lianxiang.work/work1/Main.java
@@ -0,0 +1,15 @@
+package work1;
+
+
+/**
+ * TODO
+ *
+ * @author 李响
+ * @email zoroikuzo@qq.com
+ * @date 2021/8/17 16:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!!");
+    }
+}
